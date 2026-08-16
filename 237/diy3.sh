cc6e482d6d5fc493a3c2a8cafcdedca299c45c34
general-bean <66106767+general-bean@users.noreply.github.com>
2026-08-17 06:10:31 +0800

    Create diy3.sh

---
+++ 237/diy3.sh
@@ -0,0 +1,32 @@
+# echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+# 上网时间控制
+# git clone https://github.com/sirpdboy/luci-app-timecontrol package/luci-app-timecontrol
+
+# 一键分区
+# git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+
+
+# 任务设置taskplan
+# git clone https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan
+
+# luci-app-wizard
+# git clone https://github.com/sirpdboy/luci-app-netwizard.git package/luci-app-netwizard
+
+# 看门狗
+# git clone https://github.com/sirpdboy/luci-app-watchdog package/watchdog
+
+# git clone https://github.com/papagaye744/luci-theme-design package/luci-theme-design
+
+# echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+# echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.defaul
+rm -rf feeds/packages/net/open-app-filter
+# store商店
+git clone https://github.com/linkease/istore.git package/istore
+
+# oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+# lucky大吉
+# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+
+echo -e "\n&wed2 {\n\tstatus = \"disabled\";\n};" >> target/linux/mediatek/dts/mt7986a-netcore-n60-pro.dts
